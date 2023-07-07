36e00a9fa07ddf701f20f4697e12d58cc1ad135b
Sean Tolbert <sean@seans-mbp.mynetworksettings.com>
2023-07-06 22:55:34 -0400

    init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+type ValueProps = {
+  label: string;
+  value: string;
+};
+
+const WatchDataBox = ({ label, value }: ValueProps) => (
+  <View style={styles.valueContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <Text style={styles.value}>{value}</Text>
+  </View>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{ flexDirection: "row", gap: 25, flexWrap: "wrap" }}>
+        <WatchDataBox label="Steps" value="1231" />
+        <WatchDataBox label="Distance" value="2.4 mi" />
+      </View>
+
+      <WatchDataBox label="Flights" value="29" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    justifyContent: "center",
+    padding: 10,
+    // gap: 20,
+  },
+  valueContainer: {
+    minWidth: "40%",
+    // marginVertical: 10,
+  },
+  label: {
+    color: "white",
+    fontSize: 20,
+  },
+  value: {
+    fontSize: 35,
+    color: "#afb3be",
+    fontWeight: "500",
   },
 });
